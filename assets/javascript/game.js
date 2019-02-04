80f0211719ff830b2e3938ed291caca36fe49036
Chris Warner <chriswarner@Chriss-MBP.fios-router.home>
2019-02-03 19:50:44 -0500

    Working Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var Guess;
+var Wins;
+var Losses;
+var GuessesLeft;
+var GuessesSoFar;
+var MaxGuesses = 10;
+alert("I'm JavaScript!");
+console.log("this is working");
+document.onkeyup = function (event) {}
