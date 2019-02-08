35a3c3a2bc2404c8cc83340d7d63fb55a987fb38
Chris Warner <chriswarner@Chriss-MacBook-Pro.local>
2019-02-07 21:34:34 -0500

    updated code base

--- javascript/game.js
+++ javascript/game.js
@@ -4,11 +4,11 @@ var losses = 0;
 var score_guessesLeft = 10;
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var score_guessesSoFar = [];
-//Keypresses
+
 document.onkeyup = function (event) {
     var keypress = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = newLetter();
-    score_guessesSoFar.push(keypress);
+newLetter();
     if (keypress == computerGuess) {
         wins++;
         alert("You won!");
@@ -21,17 +21,26 @@ document.onkeyup = function (event) {
     } else if (keypress !== computerGuess) {
         score_guessesLeft--;
     }
+    score_guessesSoFar.push(keypress);
+    
     //Write to the HTML file.
-    Update_HTML();
+    updateDisplay();
 }
+function Refresh() {
+    score_guessesSoFar = [];
+    guessesLeft = 9;
+    newLetter();
+    score_guessesLeft=0;
+    score_guessesSoFar=0;
 
-function Update_HTML() {
-    var updatehtml = "<p>Guess What Letter I'm Thinking Of</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + score_guessesLeft + "<p>Your Guesses so far: " +
-        score_guessesSoFar + "</p>";
-    document.querySelector("#psychic_game").innerHTML = updatehtml;
 }
-
 function newLetter() {
     //math for generating random selection.
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+function updateDisplay() {
+    document.getElementById("GuessesLeft").textContent = score_guessesLeft;
+    document.getElementById("GuessesSoFar").textContent = score_guessesSoFar;
+    document.getElementById("wins").textContent = wins;
+    document.getElementById("Losses").textContent = losses;
 }
